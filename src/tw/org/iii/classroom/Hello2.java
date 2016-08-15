433366f4343a90fb0d8101918f8c49b7d4e7297c
c4327611 <c4327611@gmail.com>
2016-08-15 16:03:47 +0800

    byte

---
+++ src/tw/org/iii/classroom/Hello2.java
@@ -0,0 +1,13 @@
+package tw.org.iii.classroom;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        byte a = 15,b = 5;
+        int c = a / b;
+        System.out.println(c);
+
+	}
+
+}
