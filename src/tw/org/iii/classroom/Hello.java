3c1a62412622209c7239337b577b6da2fabce399
c4327611 <c4327611@gmail.com>
2016-08-15 16:02:08 +0800

    Hello,Jason

---
+++ src/tw/org/iii/classroom/Hello.java
@@ -0,0 +1,11 @@
+package tw.org.iii.classroom;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello,Jason");
+	}
+
+}
