6cc76091e81e58ca34adfc9ea3b291720509f58d
c4327611 <c4327611@gmail.com>
2016-08-15 22:14:45 +0800

    Math.random()

---
+++ src/tw/org/iii/classroom/Hello6.java
@@ -0,0 +1,27 @@
+package tw.org.iii.classroom;
+
+public class Hello6 {
+
+	public static void main(String[] args) {
+		int Score = (int) (Math.random() * 101);// 0 - 100
+        Score = 85;
+        System.out.println(Score);
+
+    if (Score >= 90){
+	   System.out.println("A");
+	} else if (Score >= 80) {
+	   System.out.println("B");
+	} else if (Score >= 70) {
+	   System.out.println("C");
+	} else if (Score >= 60) {
+		System.out.println("D");
+	} else {
+		System.out.println("E");
+	}
+    System.out.println(Math.sqrt(Score) * 10); // 開根號 * 10
+	
+	// 該怎麼轉整數?
+}
+	}
+
+
