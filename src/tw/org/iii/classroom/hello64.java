042069eb5145d0fc35f590ccac4103fda9e2a6d7
renting <asdf11191030@gmail.com>
2016-09-13 21:07:06 +0800

    0913

--- src/tw/org/iii/classroom/hello64.java
+++ src/tw/org/iii/classroom/hello64.java
@@ -3,12 +3,12 @@ package tw.org.iii.classroom;
 public class hello64 {
 
 	public static void main(String[] args) {
-		// 
+		// 繼承介面
 
 	}
 
 }
-interface hello641{
+interface hello641{ //interface 介面  不能實作,但可以被繼承
 	void m1();
 		
 	}
@@ -17,6 +17,7 @@ interface hello642{
 }
 interface hello643 extends hello641, hello642 {
 	void m3();
+	
 }
 class hello644 implements hello643{
 	public void m1(){}
--- src/tw/org/iii/classroom/hello65.java
+++ src/tw/org/iii/classroom/hello65.java
@@ -9,28 +9,29 @@ import javafx.print.Collation;
 public class hello65 {
 
 	public static void main(String[] args) {
-		//HashSet<String> set = new HashSet<>();
-		String answer = CreatAnswerV2(4);
+		//產生4位數(不重複數字)
+		//HashSet<String> set = new HashSet<>();  //只能丟字串
+		String answer = createAnswerV2(4);
 		System.out.println(answer);
 	}
-	static String CreateAnswer(int n){
+	static String createAnswer(int n){
 		HashSet<Integer> set = new HashSet<>();
-		while(set.size()<n){
-			set.add((int)Math.random()*10);
+		while (set.size()<n){
+			set.add((int)(Math.random()*10));
 		}
 		StringBuffer sb = new StringBuffer(n);
-		for (Integer i:set) sb.append(i);
+		for (Integer i : set) sb.append(i);
 		return sb.toString();
 	}
-	// shuffle(list) 洗list
-	static String CreatAnswerV2 (int n){
+	
+	static String createAnswerV2 (int n){
 		LinkedList<Integer> list = new LinkedList<>();
 		for(int i=0;i<10; i++) list.add(i);
+		// shuffle(list) 洗list
 		Collections.shuffle(list);
 		StringBuffer sb = new StringBuffer(n);
 		for(int i=0;i<n;i++) sb.append(list.get(i));
 		return sb.toString();
-				
-				
-}
-}
+		
+		}
+	}
